02651efcb5975f70d376050b7865c11970694b10
Alaeddine Jmila <alaeddine.jmila@genesys.com>
2025-03-30 12:37:27 +0100

    Add dynamic port detection

--- cmd/phisiolog/main.go
+++ cmd/phisiolog/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
@@ -11,9 +13,15 @@ import (
 func main() {
 	log.SetReportCaller(true)
 	r := chi.NewRouter()
-	port := 5000
 
-	fmt.Printf("Server listening at port %v ...\n", port)
+	port := 5000
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		if p, err := strconv.Atoi(envPort); err == nil {
+			port = p
+		}
+	}
+
+	fmt.Printf("Server listening at port %d...\n", port)
 	
 	address := fmt.Sprintf("localhost:%d", port)
 	if err:= http.ListenAndServe(address, r); err != nil {
