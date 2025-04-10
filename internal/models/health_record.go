d6e80b140cc00950676550ed023570d4d535cfe8
Alaeddine Jmila <alaeddine.jmila@genesys.com>
2025-04-10 09:52:03 +0100

    Separate API structs and DB structs

--- internal/models/health_record.go
+++ internal/models/health_record.go
@@ -27,35 +27,67 @@ const (
 	Variable	Severity = "variable"
 )
 
+type UserDB struct {
+	ID		string	`sql:"id"`
+	Name	string	`sql:"name"`
+} 
 type User struct {
-	ID string 	`json:"id"`
-	Name string `json:"name"`
+	ID		string	`json:"id"`
+	Name	string	`json:"name"`
 } 
 
+type SymptomDB struct {
+	ID				string		`sql:"id"`
+	HealthRecordID	string		`sql:"health_record_id"`
+	Name			string		`sql:"name"`
+	StartDate		time.Time	`sql:"start_date"`
+}
 type Symptom struct {
-	ID string 	`json:"id"`
-	Name string			`json:"name"`
-	StartDate time.Time `json:"startDate"`
+	ID			string		`json:"id"`
+	Name		string		`json:"name"`
+	StartDate	time.Time	`json:"startDate"`
 }
 
+type MedicalConsultationDB struct {
+	ID				string		`sql:"id"`
+	HealthRecordID	string		`sql:"health_record_id"`
+	Consultant		string		`sql:"consultant"`
+	Date			time.Time	`sql:"date"`
+	Diagnosis		string		`sql:"diagnosis"`
+	FollowUpActions	[]string	`sql:"follow_up_actions"`
+}
 type MedicalConsultation struct {
-	ID string 	`json:"id"`
-	Consultant string 		 `json:"consultant"`
-	Date time.Time			 `json:"date"`
-	Diagnosis string		 `json:"diagnosis"`
-	FollowUpActions []string `json:"followUpActions"`
+	ID				string		`json:"id"`
+	Consultant		string		`json:"consultant"`
+	Date			time.Time	`json:"date"`
+	Diagnosis		string		`json:"diagnosis"`
+	FollowUpActions	[]string	`json:"followUpActions"`
+}
+
+type HealthRecordDB struct {
+	ID 				string		`sql:"id"`
+	UserID			string		`sql:"user_id"`
+	ParentRecordID	string		`sql:"parent_record_id"`
+	Description		string		`sql:"description"`
+	Progress		Progress	`sql:"progress"`	
+	Improvement		Improvement	`sql:"improvement"`
+	Severity 		Severity	`sql:"severity"`
+	TreatmentsTried []string 	`sql:"treatments_tried"`
+	CreatedAt		time.Time	`sql:"created_at"`
+	UpdatedAt		time.Time	`sql:"updated_at"`
 }
 
 type HealthRecord struct {
-	ID string 	`json:"id"`
-	User User					 				`json:"user"`	
-	Description string		 					`json:"description"`
-	Progress string			 					`json:"progress"`
-	Improvement string		 					`json:"improvement"`
-	Severity Severity							`json:"severity"`
-	Symptoms []Symptom		 					`json:"symptoms"`
-	TreatmentsTried []string 					`json:"treatmentsTried"`
-	MedicalConsultations []MedicalConsultation  `json:"medicalConsultations"`
-	CreatedAt time.Time							`json:"createdAt"`
-	UpdatedAt time.Time							`json:"updatedAt"`
+	ID 						string 					`json:"id"`
+	User 					User					`json:"user"`
+	Description 			string		 			`json:"description"`
+	Progress 				Progress				`json:"progress"`
+	Improvement				Improvement				`json:"improvement"`
+	Severity 				Severity				`json:"severity"`
+	Symptoms				[]Symptom				`json:"symptoms"`
+	TreatmentsTried 		[]string 				`json:"treatmentsTried"`
+	MedicalConsultations	[]MedicalConsultation	`json:"medicalConsultations"`
+	Updates					[]HealthRecord			`json:"updates"`
+	CreatedAt 				time.Time				`json:"createdAt"`
+	UpdatedAt 				time.Time				`json:"updatedAt"`
 }
