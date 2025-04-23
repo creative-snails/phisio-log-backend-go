2f3457ffed13659ef57c3c01b2fac5dce47a9402
Alaeddine Jmila <jmila.aladin@gmail.com>
2025-04-23 09:05:36 +0100

    Revert "3 implement health record model and db setup" (#14)

--- cmd/phisiolog/main.go
+++ cmd/phisiolog/main.go
@@ -1,42 +1,16 @@
 package main
 
 import (
-	"github.com/creative-snails/phisio-log-backend-go/config"
-	"github.com/creative-snails/phisio-log-backend-go/internal/handlers"
-	"github.com/creative-snails/phisio-log-backend-go/internal/services"
 	"github.com/creative-snails/phisio-log-backend-go/internal/startup"
 	"github.com/go-chi/chi"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	// Load configuration
-	config, err := config.LoadConfig("config/config.yaml")
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-	
-	log.SetReportCaller(true)
-	
-	// Connect to database
-	queries, err := startup.InitializeDB(config.Database)
-	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-
 	r := chi.NewRouter()
-	// Register routes before initializing the server
-	healthRecordService := services.NewHealthRecordService(queries)
-	if healthRecordService == nil {
-		log.Fatalf("Failed to create health record service")
-	}
 
-	h := handlers.NewHandler(healthRecordService)
-	if h == nil {
-		log.Fatal("Failed to create handler")
-	}
-	startup.Routes(r, h)
+	// Register routes before initializing the server
+	startup.Routes(r)
 
 	// Initialize server
-	startup.Server(r, config.Server)
+	startup.Server(r)
 }
--- internal/startup/routes.go
+++ internal/startup/routes.go
@@ -1,13 +1,10 @@
 package startup
 
 import (
-	"github.com/creative-snails/phisio-log-backend-go/internal/handlers"
 	"github.com/creative-snails/phisio-log-backend-go/internal/routes"
 	"github.com/go-chi/chi"
 )
 
-func Routes(r *chi.Mux, handler *handlers.Handler) {
-	r.Route("/api/health-records", func(r chi.Router) {
-		routes.HealthRecords(r, handler)
-	})
+func Routes(r *chi.Mux) {
+	r.Route("/api/health-records", routes.HealthRecords)
 }
