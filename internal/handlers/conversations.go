96c3d45b74ecb2c54754559756d17cbd3804d9ba
Alaeddine Jmila <alaeddine.jmila@genesys.com>
2025-05-15 07:51:53 +0100

    Add Conversation struct

---
+++ internal/handlers/conversations.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"time"
+
+	"github.com/creative-snails/phisio-log-backend-go/internal/services"
+	"github.com/google/uuid"
+)
+
+type Conversation struct {
+	ID				uuid.UUID
+	History			[]services.Message
+	LastAccessed	time.Time
+}
+
+func NewConversation (systemPrompt string) *Conversation {
+	conversation := &Conversation{}
+
+	conversation.ID = uuid.New()
+	conversation.History = []services.Message{{Role: "system", Content: systemPrompt}}
+	conversation.LastAccessed = time.Now()
+
+	return conversation
+}
+
+var Conversations = make(map[uuid.UUID]Conversation)
